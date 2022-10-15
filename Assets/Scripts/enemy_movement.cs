5ee88c4f4f29c7c78f6f19d7b6184dc9614d8773
UnstableGrooot <115672152+UnstableGrooot@users.noreply.github.com>
2022-10-15 00:31:13 -0400

    Jumping fix
    This changes how jumping works to prevent flying off into space also changes enemy ai to use velocity instead of position.

--- Assets/Scripts/enemy_movement.cs
+++ Assets/Scripts/enemy_movement.cs
@@ -7,7 +7,9 @@ public class enemy_movement : MonoBehaviour
 {
     public float speed = 3f;
     public Transform billy_pos;
- 
+    public Rigidbody2D rb;
+    private Vector2 velocity;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,20 +18,26 @@ public class enemy_movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-       
+        velocity = new Vector2(0, rb.velocity.y);
+
         if (transform.position.x > billy_pos.position.x)
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            //move and flip sprite to face right
+            velocity += Vector2.left;
             transform.localScale = new Vector3(-1, 1, 1);
         }
         if (transform.position.x <= billy_pos.position.x)
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            //move and flip sprite to face left
+            velocity += Vector2.right;
             transform.localScale = new Vector3(1, 1, 1);
         }
 
+        velocity.x *= speed;
+
+        rb.velocity = velocity;
+
     }
-  
 }
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,4 +1,5 @@
 using Packages.Rider.Editor.UnitTesting;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class movement : MonoBehaviour
     public BoxCollider2D player_collider;
     private Vector2 velocity;
     public LayerMask ground_layer;
+    public bool grounded = true;
+    public bool jumping = false;
 
 
     // Start is called before the first frame update
@@ -26,30 +29,46 @@ public class movement : MonoBehaviour
 
         if (Input.GetKey("a"))
         {   
-            velocity.x -= speed;
-            transform.localScale = new Vector3(-1, 1, 1);
+            velocity += Vector2.left;
         }
         if(Input.GetKey("d"))
         {
-            velocity.x += speed;
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        if (Input.GetKey("space") && is_grounded())
-        {
-            velocity.y += jump_height;
+            velocity += Vector2.right;
         }
 
+        //flip sprite based on what direction player is going
+        if (velocity.x != 0 && transform.localScale.x != velocity.x)
+            transform.localScale = new Vector3(velocity.x, 1, 1);
+
+        velocity.x *= speed;
+
         
+        //the jumping state can only be changed when the ground state changes
+        //used to prevent multiple jumps from happening before the players clears the is_grounded() check range
+        bool was_grounded = grounded;
+        grounded = is_grounded();
+        if (grounded != was_grounded)
+        {
+            jumping = !grounded;
+        }
+
+
+        if (Input.GetKey("space") && !jumping)
+        {
+            Debug.Log("jump");
+            velocity = Vector2.up * jump_height;
+            jumping = true;
+        }
+
+        rb.velocity = velocity;
+
     }
 
     private bool is_grounded()
     {
-        return Physics2D.BoxCast(player_collider.bounds.center, player_collider.bounds.size, 0f, Vector2.down,0.1f, ground_layer);
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = velocity;
+        bool what = Physics2D.BoxCast(player_collider.bounds.center, player_collider.bounds.size, 0f, Vector2.down,0.1f, ground_layer);
+         
+        return what;
     }
 
 }
