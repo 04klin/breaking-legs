364e6da9252cf32023c76f5d09a3223039b4982f
muchconfusion <2004klin@gmail.com>
2022-10-16 01:06:07 -0400

    Spawning Enemies

--- Assets/Scripts/death.cs
+++ Assets/Scripts/death.cs
@@ -20,9 +20,10 @@ public class death : MonoBehaviour
         
     }
 
-
+    //trigger from collision.
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //change to death within the if statement
         if(collision.gameObject.tag == "Enemy")
         {
             Debug.Log("QQQQQQQQQQ");
